1f545cb88720a1fb49c9ff1bf12ea6efa4fc41fc
Prafull100 <125451964+Prafull100@users.noreply.github.com>
2023-04-24 18:08:45 +0530

    Update printEmail.groovy

--- vars/printEmail.groovy
+++ vars/printEmail.groovy
@@ -1,17 +1,5 @@
 def call() {
-  // Get current user and job name
-  def user = getCurrentUser()
-  def jobName = getCurrentJobName()
-
-  // Print email variable with user and job name
-  def email = "${user}@mycompany.com"
-  echo "Email: \${var_email} (User: ${user}, Job: ${jobName})"
-}
-
-def getCurrentUser() {
-  return env.USER // Retrieve the current user from the environment variable
-}
-
-def getCurrentJobName() {
-  return env.JOB_NAME // Retrieve the current job name from the environment variable
+  println("var_email: ${var_email}")
+    println("Current login user: ${env.USER}")
+    println("Current job name: ${env.JOB_NAME}")
 }
